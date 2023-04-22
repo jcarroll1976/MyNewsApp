98fd98233108d0c44fecd3e41a331c3adae300ad
Joshua Carroll <carrolljosh1976@gmail.com>
2023-04-22 19:01:30 -0400

    Added Header and Navbar, styling for news items

--- src/App.tsx
+++ src/App.tsx
@@ -14,14 +14,28 @@ function App() {
 
   return (
     <div className="App">
+      <header className='newsHeader'><h1 className='headerTitle'>My Daily News</h1></header>
+      <nav>
+        <ul className='navList'>
+          <li className='navListItem'>News</li>
+          <li className='navListItem'>Sports</li>
+          <li className='navListItem'>Tech</li>
+          <li className='navListItem'>Entertainment</li>
+          <li className='navListItem'>Gaming</li>
+          <li className='navListItem'>Health</li>
+        </ul>
+      </nav>
+      <div className='grid-container'>
       {articles.map((article,i) =>
-      <div>
+      <div className='grid-item'>
       <p key={i}>{article.source.name}</p>
       <p>{article.author}</p>
       <p>{article.title}</p>
       <p><img className="newsImage" src={article.urlToImage} alt=''/></p>
+      <p>{article.content}</p>
+      <p><a>{article.url}</a></p>
       </div>)}
-      
+      </div>
     </div>
   );
 }
